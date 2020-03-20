32d76faf59bfef78ec382daf30c76ec1db6970fb
kulvirvirk <kvirkdev@gmail.com>
2020-03-20 02:37:51 +0000

    added some_function that just returns a int value

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-
+"""Python resolves names using the so-called LEGB rule, which is named after the Python scope for names. 
+The letters in LEGB stand for Local, Enclosing, Global, and Built-in."""
 
 a = 1
+b = 2
+def some_function():
+  b = 5
+  print(b)
+  return b
+
